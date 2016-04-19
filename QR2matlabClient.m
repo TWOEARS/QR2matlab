bf1627e703f7bdc2cacb8c82b71a4e1fd50364d9
Antonyo Musabini <musabini.antonyo@gmail.com>
2016-04-19 10:50:33 +0200

    buffer added succesfully

--- QR2matlabClient.m
+++ QR2matlabClient.m
@@ -2,12 +2,12 @@ addpath(genpath('~/openrobots/lib/matlab'));
 
 client = genomix.client;
 QR2matlab = client.load('QR2matlab');
-connection = QR2matlab.connect_port('dataIn', '/visp_auto_tracker/code_message');
+QR2matlab.connect_port('dataIn', '/visp_auto_tracker/code_message' );
 
-publish = QR2matlab.Publish('-a',10);
+publish = QR2matlab.Publish(10);
 
-while(1)
-    result = QR2matlab.dataOut();
-    disp(result.dataOut.message.data);
-    pause(0.1);
-end
+result = QR2matlab.dataOut();
+disp(result.dataOut.message.data);
+
+stop = QR2matlab.Stop();
+kill = QR2matlab.kill();
--- codels/QR2matlab_publish_codels.c
+++ codels/QR2matlab_publish_codels.c
@@ -3,13 +3,11 @@
 #include "QR2matlab_c_types.h"
 
 #include <sys/time.h>
-
 #include <string.h>
 #include <stdio.h>
 
 struct timeval tv;
-static volatile int32_t ii;
-
+static volatile uint32_t ii; 
 /* --- Task publish ----------------------------------------------------- */
 
 
@@ -19,11 +17,10 @@ static volatile int32_t ii;
  * Yields to QR2matlab_ether.
  */
 genom_event
-init(QR2matlab_ids *ids, const QR2matlab_dataOut *dataOut,
-     genom_context self)
+init(const QR2matlab_dataOut *dataOut, genom_context self)
 {
-  ids->data._length = 0;
-
+  dataOut->data(self)->header.seq = 0;
+  dataOut->write( self );
   return QR2matlab_ether;
 }
 
@@ -33,29 +30,28 @@ init(QR2matlab_ids *ids, const QR2matlab_dataOut *dataOut,
 /** Codel start of activity Publish.
  *
  * Triggered by QR2matlab_start.
- * Yields to QR2matlab_exec.
+ * Yields to QR2matlab_readQR.
  * Throws QR2matlab_e_noMemory.
  */
 genom_event
-start(int32_t bufferSize, QR2matlab_ids *ids, genom_context self)
+start(uint32_t bufferSize, QR2matlab_ids *ids, genom_context self)
 {
-  if ( ids->data._length != bufferSize ) {
-	  ids->data._length = bufferSize;
-	  if ( genom_sequence_reserve(&(ids->data), bufferSize) )
-	  return QR2matlab_e_noMemory( self );
-  }
+  ids->data._length = bufferSize;
+  if (genom_sequence_reserve(&(ids->data), bufferSize) )
+	 return QR2matlab_e_noMemory( self );
+	 
+  int32_t iii;
+  for ( iii = 0 ; iii < ids->data._length ; iii++ ) 
+    strcpy(ids->data._buffer[iii], " ");
 
-  for ( int32_t iii = 0 ; iii < ids->data._length ; iii++ )
-	ids->data._buffer[iii] = " ";
-	  
   ii = 0;
-  return QR2matlab_exec;
+  return QR2matlab_readQR;
 }
 
 /** Codel readQR of activity Publish.
  *
- * Triggered by QR2matlab_exec.
- * Yields to QR2matlab_pause_exec, QR2matlab_publish.
+ * Triggered by QR2matlab_readQR.
+ * Yields to QR2matlab_pause_readQR, QR2matlab_writeQR.
  * Throws QR2matlab_e_noMemory.
  */
 genom_event
@@ -63,31 +59,41 @@ readQR(const QR2matlab_dataIn *dataIn, QR2matlab_ids *ids,
        genom_context self)
 {
   dataIn->read(self);
-  
-  if ( dataIn->data(self)->data != NULL )  {
-		ids->data._buffer[ii] = dataIn->data( self )->data;
-  }
-  
-  ii++;
-  if ( ii < ids->data._length )
-	return QR2matlab_pause_exec;
-  else	  
-    return QR2matlab_publish;
+  if(dataIn->data(self)->data != NULL)
+	  strcpy(ids->data._buffer[ii], dataIn->data( self )->data);
+
+  if (ii++ < ids->data._length-1 )
+	  return QR2matlab_pause_readQR;
+  else 
+	  return QR2matlab_writeQR;
 }
 
 /** Codel writeQR of activity Publish.
  *
- * Triggered by QR2matlab_publish.
+ * Triggered by QR2matlab_writeQR.
  * Yields to QR2matlab_ether.
  * Throws QR2matlab_e_noMemory.
  */
 genom_event
-writeQR(QR2matlab_ids *ids, const QR2matlab_dataOut *dataOut,
+writeQR(const QR2matlab_ids *ids, const QR2matlab_dataOut *dataOut,
         genom_context self)
 {
-  for ( int32_t iii = 0 ; iii < ids->data._length ; iii++ )
-	printf("%d - % s", iii, ids->data._buffer[iii]);
+  dataOut->data(self)->message.data._length = ids->data._length;
+  if ( genom_sequence_reserve(&(dataOut->data(self)->message.data), ids->data._length) )
+	 return QR2matlab_e_noMemory( self );
+
+  int32_t iii;      
+  for ( iii = 0 ; iii < ids->data._length ; iii++ )
+	if ( ids->data._buffer[iii] != NULL ) 
+		strcpy(dataOut->data(self)->message.data._buffer[iii], ids->data._buffer[iii]);
 		
+  gettimeofday(&tv, NULL);
+	 
+  dataOut->data(self)->header.seq += 1;
+  dataOut->data(self)->header.stamp.sec = tv.tv_sec;
+  dataOut->data(self)->header.stamp.usec = tv.tv_usec;
+		  
+  dataOut->write( self );
   return QR2matlab_ether;
 }
 
@@ -98,8 +104,11 @@ writeQR(QR2matlab_ids *ids, const QR2matlab_dataOut *dataOut,
  * Throws QR2matlab_e_noMemory.
  */
 genom_event
-stop(const QR2matlab_dataOut *dataOut, genom_context self)
+stop(QR2matlab_ids *ids, const QR2matlab_dataOut *dataOut,
+     genom_context self)
 {
-  // dataOut->data(self)->message.data._release( dataOut->data(self)->message.data._buffer );
+  ids->data._release( ids->data._buffer );
+  dataOut->data(self)->message.data._release( dataOut->data(self)->message.data._buffer );
+  
   return QR2matlab_ether;
 }
